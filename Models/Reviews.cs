4bb3f4ff9c83c54f7c748dd0a3bdfd37fd55b2aa
ZaveriAum <aumzaveri06@gmail.com>
2024-02-12 10:03:31 -0500

    Updated some model.

---
+++ Models/Reviews.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP2139_Assignment1.Models
+{
+    public class Reviews
+    {
+        [Key]
+        public int RatingId { get; set; }
+
+        [Required]
+        public string Review {  get; set; }
+
+        public string CarPlateNumber { get; set; }
+
+        public Car? Car {  get; set; }
+        
+        public string? RoomId { get; set; }
+
+        public Room? Room { get; set; }
+
+        public string? HotelId { get; set; }
+        public Hotel? Hotel { get; set; }
+    }
+}
---
+++ Models/Room.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP2139_Assignment1.Models
+{
+    public class Room
+    {
+        [Key]
+        public string RoomId { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Highlights { get; set; }
+
+        [Required]
+        public int Price { get; set; }
+
+        [Required]
+        public int Rating { get; set; }
+    }
+}
