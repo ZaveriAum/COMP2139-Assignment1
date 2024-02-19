11a290a169f4a20d8e415ca817b0a2462c90808d
ZaveriAum <aumzaveri06@gmail.com>
2024-02-18 23:03:05 -0500

    #Keeping up with Aum Branch.

---
+++ Models/Photo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP2139_Assignment1.Models
+{
+    public class Photo
+    {
+        [Key]
+        public int photoId { get; set; }
+
+        [Required]
+        public string photoLocation { get; set; }
+
+        public string? CarPlateNumber { get; set; }
+
+        public Car? Car { get; set; }
+
+        public string? RoomId { get; set; }
+
+        public Room? Room { get; set; }
+
+        public string? HotelId { get; set; }
+        public Hotel? Hotel { get; set; }
+    }
+}
