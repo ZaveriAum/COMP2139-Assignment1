d2f1c0a6ce63e20c75d707315928be51cbfbc0d1
ZaveriAum <aumzaveri06@gmail.com>
2024-02-19 10:29:52 -0500

    Flight Number validation Added

--- Models/Flight.cs
+++ Models/Flight.cs
@@ -2,33 +2,34 @@
 
 namespace COMP2139_Assignment1.Models
 {
-    public class Flight
-    {
-        [Key]
-        public int FlightId { get; set; }
+	public class Flight
+	{
+		[Key]
+		public int FlightId { get; set; }
 
-        [Required]
-        public string FlightNumber { get; set; }
+		[Required]
+		[RegularExpression(@"^[A-Z]{2}\d{3,4}$", ErrorMessage = "Flight number must start with two uppercase letters followed by three or four digits.")]
+		public string FlightNumber { get; set; }
 
-        [Required]
-        public string Airline { get; set;}
+		[Required]
+		public string Airline { get; set; }
 
-        [Required]
-        public DateTime ArrivalTime { get; set; }
+		[Required]
+		public DateTime ArrivalTime { get; set; }
 
-        [Required]
-        public DateTime DepartureTime { get; set; }
+		[Required]
+		public DateTime DepartureTime { get; set; }
 
-        [Required]
-        public int Price { get; set; }
+		[Required]
+		public int Price { get; set; }
 
-        [Required]
-        public string From {  get; set; }
+		[Required]
+		public string From { get; set; }
 
-        [Required]
-        public string To { get; set; }
+		[Required]
+		public string To { get; set; }
 
-        [Required]
-        public int Seats { get; set; }
-    }
+		[Required]
+		public int Seats { get; set; }
+	}
 }
