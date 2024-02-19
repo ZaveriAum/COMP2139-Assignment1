671ebd3e150339affed0efc60636890697bbf2f8
Mia Truong <truongthuykyduyen@gmail.com>
2024-02-19 13:28:01 -0500

    check2

--- Models/Hotel.cs
+++ Models/Hotel.cs
@@ -2,5 +2,25 @@
 {
     public class Hotel
     {
+<<<<<<< Updated upstream
+=======
+        [Key]
+        public int HotelId { get; set; }
+
+        [Required]
+        public string HotelName { get; set;}
+
+        [Required]
+        public string HotelLocation { get; set;}
+
+        [Required]
+        public string Features { get; set;}
+
+        [Required]
+        public int? Rating { get; set;}
+
+        [Required]
+        public string Description { get; set;}
+>>>>>>> Stashed changes
     }
 }
---
+++ Models/Room.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP2139_Assignment1.Models
+{
+    public class Room
+    {
+        [Key]
+        public string RoomId { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Highlights { get; set; }
+
+        [Required]
+        public int Price { get; set; }
+
+        public int? Rating { get; set; }
+        // Foreign key for Hotel
+        public int HotelId { get; set; }
+
+        // Navigation property
+        public Hotel? Hotel { get; set; }
+    }
+}
