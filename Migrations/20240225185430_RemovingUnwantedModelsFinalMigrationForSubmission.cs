251c54a4b55b1d5ad54aac0638c7b2f73e33d3b2
ZaveriAum <aumzaveri06@gmail.com>
2024-02-25 14:00:26 -0500

    Unwanted Tables have been droopped.

---
+++ Migrations/20240225185430_RemovingUnwantedModelsFinalMigrationForSubmission.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace COMP2139_Assignment1.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovingUnwantedModelsFinalMigrationForSubmission : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PhotoCars");
+            migrationBuilder.DropTable(
+                name: "PhotoRooms");
+            migrationBuilder.DropTable(
+                name: "PhotoHotels");
+            migrationBuilder.DropTable(
+                name: "ReviewCars");
+            migrationBuilder.DropTable(
+                name: "ReviewRooms");
+            migrationBuilder.DropTable(
+                name: "ReviewHotels");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
