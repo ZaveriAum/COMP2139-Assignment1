93a37cb902af731ad0c2b6b725ccb44f7e3814dc
ElioFezollari <fezollarielio@gmail.com>
2024-04-12 17:47:29 -0400

    Fixed bugs that did not allow Admin to view Users Profile

--- Areas/NorthPole/Controllers/UserController.cs
+++ Areas/NorthPole/Controllers/UserController.cs
@@ -1,14 +1,34 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using COMP2139_Assignment1.Areas.NorthPole.Models;
+using COMP2139_Assignment1.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace COMP2139_Assignment1.Areas.NorthPole.Controllers
 {
-	[Authorize("Admin,SuperAdmin")]
+	[Area("NorthPole")]
+	[Route("[area]/[controller]/[action]")]
+	[Authorize(Roles ="Admin,SuperAdmin")]
 	public class UserController : Controller
 	{
-		public IActionResult Index()
+		private readonly ApplicationDbContext _context;
+
+		public UserController(ApplicationDbContext context)
 		{
-			return View();
+			_context = context;
+		}
+		public async Task<IActionResult> Index(string userId)
+		{
+			NorthPoleUser user = await _context.Users.FindAsync(userId);
+
+			if (user != null)
+			{
+				return View(user);
+			}
+			else
+			{
+				return NotFound();
+			}
 		}
 	}
 }
