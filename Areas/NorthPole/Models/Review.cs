e4987eaa1a0e5a2b3d27a4542df455180a62f249
ElioFezollari <fezollarielio@gmail.com>
2024-04-12 16:05:05 -0400

    Added Review Model

---
+++ Areas/NorthPole/Models/Review.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP2139_Assignment1.Areas.NorthPole.Models
+{
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+        [Range(0, 10, ErrorMessage = "Value must be between 0 and 10")]
+        [Required]
+        public int Rating { get; set; }
+        [Range(0, 500, ErrorMessage = "Comment must be between 0 and 500")]
+        public string? Comment {  get; set; }
+    }
+}
