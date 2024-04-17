e727dbabd7f3211b69aa9e75e498dd0e9508ee50
ElioFezollari <fezollarielio@gmail.com>
2024-04-16 20:16:14 -0400

    Added FlightNr and HotelNr and fixed TwoFactorAuthentication Table

--- Areas/NorthPole/Models/NorthPoleUser.cs
+++ Areas/NorthPole/Models/NorthPoleUser.cs
@@ -16,6 +16,11 @@ namespace COMP2139_Assignment1.Areas.NorthPole.Models
         [Required(ErrorMessage = "Username change limit is required.")]
         [Range(0, int.MaxValue, ErrorMessage = "Username change limit must be a positive number.")]
         public int UsernameChangeLimit { get; set; }
+        [StringLength(25, ErrorMessage = "Flyer number must be between 1 and 25 characters.", MinimumLength = 1)]
+        public string? FrequentFlyerNumber { get; set; }
+        [StringLength(25, ErrorMessage = "Hotel ID must be between 1 and 25 characters.", MinimumLength = 1)]
+
+        public string? HotelLoyaltyNumber {  get; set; }
 
         // ProfilePicture and Address can be optional, so no Data Annotations are necessary
         public byte[]? ProfilePicture { get; set; }
---
+++ Migrations/20240417000915_addedFlightNumbersandHotelNumbersforUser.cs
@@ -0,0 +1,190 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace COMP2139_Assignment1.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedFlightNumbersandHotelNumbersforUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FrequentFlyerNumber",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "HotelLoyaltyNumber",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                schema: "Identity",
+                table: "HotelReviews",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                schema: "Identity",
+                table: "FlightReviews",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                schema: "Identity",
+                table: "CarReviews",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FrequentFlyerNumber",
+                schema: "Identity",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "HotelLoyaltyNumber",
+                schema: "Identity",
+                table: "User");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                schema: "Identity",
+                table: "HotelReviews",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                schema: "Identity",
+                table: "FlightReviews",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                schema: "Identity",
+                table: "CarReviews",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+        }
+    }
+}
